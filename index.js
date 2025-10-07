1905a9bb4391b8f4f6955d8ab6c6569e41acbed7
Juan Machaca Espinoza <juan.machaca@tecsup.edu.pe>
2025-10-07 13:47:06 -0500

    Agregamos package.json

---
+++ index.js
@@ -0,0 +1,14 @@
+requestAnimationFrame('dotenv').config()
+const http= require('http')
+
+function requestController(){
+    console.log('Bienvenidos al curso')
+}
+
+const server=http.createServer(requestController)
+
+const PORT= process.env.PORT || 3000
+
+server.listen(PORT,()=>{
+    console.log(`Servidor escuchando en el puerto ${PORT}`)
+})
